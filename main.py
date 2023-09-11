abca8507b3a3d782cccc2f54a0326eb141465725
Hakan Özcan <hakanozcan2534@gmail.com>
2023-09-11 23:42:30 +0300

    started

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("light blue")
+turtle.shape("turtle")
+turtle.shapesize(5, 5, 12)
+
+
+
+
+turtle.mainloop()
+
+
+
+
+#pos() x ve y kordinatlarına ışınlar
